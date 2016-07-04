34e1b9c6ff8eb27b016f142067ca3f5cb7b683a6
IcePad <haman_g@hotmail.com>
2016-07-05 09:51:30 +1200

    Added jump feature to player object
    Added jumper feature to player object as well as renamed some of the
    objects.

---
+++ flappy/Assets/playerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		//GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+
+		{
+			if (Input.GetKeyDown ("space")) {
+				Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+
+				GetComponent<Rigidbody2D>().AddForce (jump);
+			}
+		}
+	}
+}
