0ee7027f7885e0b95ebd42bb65a4274abcac3972
splatsid <siddu_787@hotmail.com>
2016-07-05 23:10:27 +1200

    Added animation
    walks and jumps fine probably need to change speed and mass as we go

--- flappy/Assets/playerScript.cs
+++ flappy/Assets/playerScript.cs
@@ -2,27 +2,39 @@
 using System.Collections;
 
 public class playerScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    public float speed = 2;
+    public float maxSpeed;
+    private Rigidbody2D rb2d;
+	void Start () { 
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = 1;
+        rb2d = GetComponent<Rigidbody2D>();
 
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        if (moveHorizontal * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(movement * speed);
+        }else
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+    }
 
-		//GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+    void Update()
+    {
+     
+        
 
-		{
-			if (Input.GetKeyDown ("space")) {
-				Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
 
-				GetComponent<Rigidbody2D>().AddForce (jump);
-			}
-		}
-	}
+            GetComponent<Rigidbody2D>().AddForce(jump);
+        }
+    }
 }
