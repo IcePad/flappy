48ab25f29fa7c3d6d7abb7d02f7b2686b0290800
splatsid <siddu_787@hotmail.com>
2016-07-08 01:05:00 +1200

    object generation
    loops around obstacle type objects, needs tweaking

---
+++ flappy/Assets/LoopAround.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopAround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Triggered" + collider.name);
+        float widthOfBgObject = collider.GetComponent<BoxCollider2D>().size.x;
+
+        Vector3 pos = collider.transform.position;
+        if (collider.tag.Equals("Obstacle"))
+        {
+            pos.x += widthOfBgObject + 20f;
+            pos.y = Random.Range(-1.72f, 2.16f);
+            collider.transform.position = pos;
+        }
+        
+    }
+}
